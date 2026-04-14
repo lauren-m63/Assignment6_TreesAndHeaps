d567b1f1e4278e333bf0948f0525784e09aa80a8
lauren-m63 <lauren.m63@yahoo.com>
2026-04-14 12:09:51 -0500

    notes for 4

--- src/Find_Element_Encrypted_Binary_Tree_4.java
+++ src/Find_Element_Encrypted_Binary_Tree_4.java
@@ -6,12 +6,22 @@ public class Find_Element_Encrypted_Binary_Tree_4 {
     so  ahve -2 is real but value is hidden girl
     then -1 is no node null
 
+    so im just rebuilding the values and go thorugh it just the once it seems like
+    dont make a new tree though just walk thorugh the array and compute new values to overwrite whats going on
+    in there
+    start at the root and then i have to do left is ugh
+    okay left 3x+1
+    right 2x+5
+    why
+    jsut the directions then i have to do checks and stuff
+
      */
 
     //public boolean find()
 
     public boolean rebuild(int[]bt, int index, int value, int target ){
 
+        return true;
     }// END REBUILD
 
 }
