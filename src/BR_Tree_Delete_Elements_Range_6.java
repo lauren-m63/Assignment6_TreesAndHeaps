7bf8b4548a3571024b08a157b43b96bd26c1d927
lauren-m63 <lauren.m63@yahoo.com>
2026-04-14 15:02:34 -0500

    notes for 6

--- src/BR_Tree_Delete_Elements_Range_6.java
+++ src/BR_Tree_Delete_Elements_Range_6.java
@@ -2,5 +2,18 @@ public class BR_Tree_Delete_Elements_Range_6 {
 
     /*
 
+    so i am given a red black tree and two values and then i have to go through the tree
+    and then delet the values tha are less than or equal to a and like jsu tin between or equal to a and b
+    so ill have to check to make sure a is less than b or can i just take the smaller one of the input no prob not imll do a check
+
+    but then im deleting so i still have to keep th black red structure ughhh and bst so
+    wait
+    no balance just the red and black proerties and then bst of course
+    do i hvae to make one black or red can it jus tbe conceptual or no i have to i think
+    do i jsut add a variable black or red to each node i guess should i do a map
+    hashmap maybe like value, color, balance
+    no balance i think
+
+
      */
 }
